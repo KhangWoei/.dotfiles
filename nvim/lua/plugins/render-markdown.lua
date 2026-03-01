ed361184a861d985d6b2fca3dbf7fd4d521fa2ae
Khang Woei <41323253+KhangWoei@users.noreply.github.com>
2026-03-01 23:02:38 +0000

    refactor markdown to be declarative

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -1,12 +1,15 @@
 -- render-markdown.nvim
 
-return {
+local render_markdown = {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    config = function()
-        require('render-markdown').setup({
-            html  = { enabled = false },
-            latex = { enabled = false },
-        })
-    end,
 }
+
+render_markdown.config = function()
+    require('render-markdown').setup({
+        html  = { enabled = false },
+        latex = { enabled = false },
+    })
+end
+
+return render_markdown
