01d9e856f874364184752d712e40edbd4e195386
Khang Woei <41323253+KhangWoei@users.noreply.github.com>
2026-03-01 22:59:30 +0000

    refactor lualine plugin to use declaritive syntax

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,15 +1,20 @@
 -- lualine.lua
 -- Add indentation guides even on blank lines
 
-return {
+local lualine = {
 	'nvim-lualine/lualine.nvim',
+}
+
+lualine.config = function()
 	-- See `:help lualine.txt`
-	opts = {
+	require('lualine').setup({
 		options = {
 			icons_enabled = true,
 			theme = 'powerline_dark',
 			component_separators = '|',
 			section_separators = '',
 		},
-	},
-}
+	})
+end
+
+return lualine
