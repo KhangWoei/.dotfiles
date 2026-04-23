32ca7680372852ea0fc36905142e34e0928ffc0a
Khang <41323253+KhangWoei@users.noreply.github.com>
2026-04-24 00:46:46 +0100

    add plugins

--- zshrc/plugins.zsh
+++ zshrc/plugins.zsh
@@ -1,4 +1,4 @@
 export ZSH="$XDG_CONFIG_HOME/oh-my-zsh"
 ZSH_THEME="sonicradish"
-plugins=(git)
+plugins=(git dotnet docker gh history nmap npm python ssh systemd terraform tmux zsh-autosuggestions zsh-syntax-highlighting)
 source $ZSH/oh-my-zsh.sh
